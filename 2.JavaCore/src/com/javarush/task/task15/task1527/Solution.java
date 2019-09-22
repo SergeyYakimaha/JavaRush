fc7b49c1ddfa582945f307cba55cb87e4ae13b31
SergeyYakimaha <s.yakimaha78@gmail.com>
2019-09-22 18:44:22 +0300

    commit

--- 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task15.task1527;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -9,9 +10,19 @@ import java.io.InputStreamReader;
 */
 
 public class Solution {
-    public static void main(String[] args) {
-        //add your code here
-    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String text = bufferedReader.readLine();
+        bufferedReader.close();
+
+        String[] stringParams = text.substring(text.indexOf("?") + 1).split("&");
+
+        for (int i = 0; i < stringParams.length; i++) {
+            String [] params = stringParams[i].split("\\s");
+
+
+            }
+        }
 
     public static void alert(double value) {
         System.out.println("double: " + value);
