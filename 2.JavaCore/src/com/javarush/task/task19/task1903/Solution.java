65ddd6670d7e733ec64f814ab90e8c46f170e791
SergeyYakimaha <s.yakimaha78@gmail.com>
2019-09-22 15:52:09 +0300

    commit

--- 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
@@ -10,13 +10,48 @@ import java.util.Map;
 public class Solution {
     public static Map<String, String> countries = new HashMap<String, String>();
 
+    static {
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
+    }
+
     public static void main(String[] args) {
-
     }
 
-    public static class IncomeDataAdapter {
-    }
+    public static class IncomeDataAdapter implements Customer, Contact {
+        private IncomeData data;
 
+        public IncomeDataAdapter(IncomeData data) {
+            this.data = data;
+        }
+
+        @Override
+        public String getCompanyName() {
+            return data.getCompany();
+        }
+
+        @Override
+        public String getCountryName() {
+            return countries.get(data.getCountryCode());
+        }
+
+        @Override
+        public String getName() {
+            return data.getContactLastName() + ", " + data.getContactFirstName();
+        }
+
+        @Override
+        public String getPhoneNumber() {
+            return "+" + data.getCountryPhoneCode() + String.format(
+                    "%010d",
+                    data.getPhoneNumber()
+            ).replaceFirst(
+                    "(\\d{3})(\\d{3})(\\d{2})(\\d{2})",
+                    "($1)$2-$3-$4"
+            );
+        }
+    }
 
     public static interface IncomeData {
         String getCountryCode();        //For example: UA
