38d594a7dd40cdcf01d7969a8c56e32b97820a3e
SergeyYakimaha <s.yakimaha78@gmail.com>
2019-09-24 16:07:27 +0300

    Commit

---
+++ MyProjects/src/Test.java
@@ -0,0 +1,41 @@
+public class Test {
+    public static class Animal {
+        public void print(){
+            System.out.println("Animal");
+        }
+
+        public void printAnimal(){
+
+        }
+    }
+
+    public class Dog extends Animal {
+        @Override
+        public void print() {
+            System.out.println("Dog");
+        }
+
+        public void printDog(){
+
+        }
+    }
+
+    public class DogShepherd extends Dog {
+        @Override
+        public void print() {
+            System.out.println("DogShepherd");
+        }
+
+        public void printDogShepherd() {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        Test.Dog dog = test.new Dog();
+    }
+
+
+
+}
