c8b9ee34b9d999176eb299e31bc32d24ad5e63e9
SergeyYakimaha <s.yakimaha78@gmail.com>
2019-09-26 11:05:42 +0300

    Commit

--- MyProjects/src/Serialization/MainClass.java
+++ MyProjects/src/Serialization/MainClass.java
@@ -28,13 +28,12 @@ public class MainClass {
     }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        ClassC c = new ClassC("aaa");
-        c.classB = new ClassB("bbb");
-        c.classCName = "ClassCName";
-        c.classBName = "ClassBName";
-        c.classAName = "ClassAName";
+        ClassC object = new ClassC("aaa");
+        object.classCName = "ClassCName";
+        object.classBName = "ClassBName";
+        object.classAName = "ClassAName";
 
-        MainClass.serialize(c);
+        MainClass.serialize(object);
 
         ClassC serializeObject = (ClassC)MainClass.deserialize();
         System.out.println(serializeObject.classB.classBName);
---
+++ MyProjects/src/Thread/Main.java
@@ -0,0 +1,14 @@
+package Thread;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        ThreadClass object = new ThreadClass();
+        Thread thread = new Thread(object);
+        ThreadTest threadTest = new ThreadTest();
+        threadTest.start();
+        thread.start();
+        Thread.sleep(100);
+        thread.interrupt();
+        threadTest.interrupt();
+    }
+}
