49bed71bdc2f3e83b5cca24b9e013f64f1100cde
SergeyYakimaha <s.yakimaha78@gmail.com>
2019-09-28 00:46:04 +0300

    commit

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task20.task2025;
+
+/* 
+Алгоритмы-числа
+*/
+public class Solution {
+    public static long[] getNumbers(long N) {
+        long[] result = null;
+        int countNumber = String.valueOf(N).length();
+
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        long[] arrayLong = getNumbers(370L);
+
+    }
+}
