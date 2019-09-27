5fb853656e18cd8820a48ede24df03cc38238e1d
SergeyYakimaha <s.yakimaha78@gmail.com>
2019-09-27 14:36:29 +0300

    Commit

--- 2.JavaCore/src/com/javarush/task/task20/task2020/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2020/Solution.java
@@ -1,6 +1,6 @@
 package com.javarush.task.task20.task2020;
 
-import java.io.PrintStream;
+import java.io.*;
 import java.util.logging.Logger;
 
 /* 
@@ -8,15 +8,38 @@ import java.util.logging.Logger;
 */
 public class Solution {
 
-    public static class Person {
+    public static final String FILE_NAME = "C:\\person.dat";
+
+    public static void serializable(Person person) throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+        objectOutputStream.writeObject(person);
+
+        objectOutputStream.close();
+        fileOutputStream.close();
+    }
+
+    public static Person deserializable() throws IOException, ClassNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream(FILE_NAME);
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+        Person person = (Person) objectInputStream.readObject();
+
+        objectInputStream.close();
+
+        return person;
+    }
+
+    public static class Person implements Serializable {
         String firstName;
         String lastName;
-        String fullName;
-        final String greeting;
+        transient String fullName;
+        transient String greeting;
         String country;
         Sex sex;
-        PrintStream outputStream;
-        Logger logger;
+        transient PrintStream outputStream;
+        transient Logger logger;
 
         Person(String firstName, String lastName, String country, Sex sex) {
             this.firstName = firstName;
@@ -28,6 +51,20 @@ public class Solution {
             this.outputStream = System.out;
             this.logger = Logger.getLogger(String.valueOf(Person.class));
         }
+
+        private void writeObject(ObjectOutputStream out) throws IOException {
+            out.defaultWriteObject();
+            out.writeObject(firstName + " " + lastName);
+            out.writeObject(greeting);
+        }
+
+        private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+            in.defaultReadObject();
+            fullName = (String)in.readObject();
+            greeting = (String)in.readObject();
+            outputStream = System.out;
+            logger = Logger.getLogger(String.valueOf(this));
+        }
     }
 
     enum Sex {
@@ -35,7 +72,19 @@ public class Solution {
         FEMALE
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Person person = new Person("Сергей", "Иванов", "Украина", Sex.MALE);
 
+        Solution.serializable(person);
+
+        Person clonePerson = Solution.deserializable();
+
+        System.out.println(clonePerson.firstName);
+        System.out.println(clonePerson.lastName);
+        System.out.println(clonePerson.fullName);
+        System.out.println(clonePerson.country);
+        System.out.println(clonePerson.greeting);
+        System.out.println(clonePerson.sex);
+        System.out.println(clonePerson.logger);
     }
 }
