71db4aff6ce82e2159c38a62f02ed9328653f2a5
SergeyYakimaha <s.yakimaha78@gmail.com>
2019-10-06 18:48:00 +0300

    commit

--- 4.JavaCollections/src/com/javarush/task/task32/task3210/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3210/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task32.task3210;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.RandomAccessFile;
 
 /* 
@@ -8,7 +10,20 @@ import java.io.RandomAccessFile;
 */
 
 public class Solution {
-    public static void main(String... args) {
+    public static void main(String... args) throws IOException {
+        RandomAccessFile randomAccessFile = new RandomAccessFile(args[0], "rw");
 
+        byte[] buffer = new byte[args[2].length()];
+        randomAccessFile.seek(Integer.parseInt(args[1]));
+        randomAccessFile.read(buffer, 0, args[2].length());
+
+        randomAccessFile.seek(randomAccessFile.length());
+
+        if (new String(buffer).equals(args[2])) {
+            randomAccessFile.write("true".getBytes());
+        } else {
+            randomAccessFile.write("false".getBytes());
+        }
+        randomAccessFile.close();
     }
 }
