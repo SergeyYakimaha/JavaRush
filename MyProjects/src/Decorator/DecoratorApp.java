d8050b31c2bc084ba132c3317ec83b879e7bbb19
SergeyYakimaha <s.yakimaha78@gmail.com>
2019-10-16 15:32:41 +0300

    Commit

--- MyProjects/src/Decorator/DecoratorApp.java
+++ MyProjects/src/Decorator/DecoratorApp.java
@@ -51,11 +51,11 @@ public class DecoratorApp {
     }
 
     public static void main(String[] args) {
-        PrinterInterface pMPChars = new addMinusDecorator(new addPlusDecorator(new PrintChars()));
+        PrinterInterface pPMPChars = new addPlusDecorator(new addMinusDecorator(new addPlusDecorator(new PrintChars())));
         PrinterInterface pPMChars = new addPlusDecorator(new addMinusDecorator(new PrintChars()));
         PrinterInterface pPNumber = new addPlusDecorator(new PrintNumber());
 
-        pMPChars.print();
+        pPMPChars.print();
         System.out.println();
         pPMChars.print();
         System.out.println();
