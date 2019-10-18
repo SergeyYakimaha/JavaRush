e2523fd458af86085279054bbd95c95dcfc670f8
SergeyYakimaha <s.yakimaha78@gmail.com>
2019-10-18 23:57:41 +0300

    commit

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -0,0 +1,58 @@
+package com.javarush.task.task37.task3707;
+
+import java.io.Serializable;
+import java.util.*;
+
+public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneable {
+
+    private static final Object PRESENT = new Object();
+
+    private transient HashMap<E, Object> map;
+
+    public AmigoSet() {
+        map = new HashMap<>();
+    }
+
+    public AmigoSet(Collection<? extends E> collection) {
+        int capacity = Math.max(16, (int) (collection.size() / .75f) + 1);
+        map = new HashMap<>(capacity);
+        addAll(collection);
+    }
+
+    @Override
+    public boolean add(Object o) {
+        return (map.put((E) o, PRESENT) == null);
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return map.keySet().iterator();
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return map.remove(o) == null;
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return super.contains(o);
+    }
+
+    //implements Serializable, Cloneable, Set
+}
