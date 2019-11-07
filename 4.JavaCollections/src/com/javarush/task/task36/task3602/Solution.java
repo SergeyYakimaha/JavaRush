99e62511f8973f833a6be4a1709f1c144aa7e21c
SergeyYakimaha <s.yakimaha78@gmail.com>
2019-11-07 14:37:35 +0200

    Commit

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3602/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task36.task3602;
+
+import java.awt.*;
+import java.lang.reflect.Modifier;
+
+/*
+Найти класс по описанию
+*/
+public class Solution {
+    public static void main(String[] args) throws ClassNotFoundException {
+        System.out.println(getExpectedClass());
+    }
+
+    private static boolean isPrivateStatic(Class<?> clazz) {
+//        1 - public
+//        2 - private
+//        4 - protected
+//        8 - static
+        return (clazz.getModifiers() == (Modifier.PRIVATE | Modifier.STATIC));
+    }
+
+    private static boolean isAssignableFrom(Class<?> clazz) {
+        return clazz.isAssignableFrom(List.class);
+    }
+
+    private static Class<?>[] getInterfeces(Class<?> clazz) {
+        return clazz.getInterfaces();
+    }
+
+    public static Class getExpectedClass() throws ClassNotFoundException {
+        Class clazz = Class.forName("java.util.Collections");
+        Class<?>[] classes = clazz.getDeclaredClasses();
+
+        for (Class<?> entryClass : classes) {
+            if (isPrivateStatic(entryClass)) {
+                System.out.println("Class: " + entryClass.getSimpleName());
+                System.out.println("Interfeces list:");
+                Class<?>[] classesInt = getInterfeces(entryClass);
+                for (Class<?> entryInt: classesInt) {
+                    System.out.println(entryInt.getSimpleName());
+                }
+                System.out.println("-----------------------------");
+            }
+//            System.out.println(entry.getModifiers());
+//            System.out.println("---------------");
+        }
+
+
+        return null;
+    }
+}
---
+++ MyProjects/src/Arrays/ArrayDemoApp.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayDemoApp {
+    List<String> stringList = new ArrayList<>();
+    int[] b = new int[5];
+
+    public static void main(String[] args) {
+
+    }
+}
