e1a406671aaf02324453d260a8619c6337905599
SergeyYakimaha <s.yakimaha78@gmail.com>
2019-11-22 20:54:51 +0200

    commit

--- 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
@@ -7,12 +7,49 @@ import java.util.Date;
 
 public class Solution {
     public static void main(String[] args) throws IOException, ParseException {
-        LogParser logParser = new LogParser(Paths.get("d:/logs/"));
+        LogParser logParser = new LogParser(Paths.get("c:/logs/"));
+
+        System.out.println("-- 1 -- ");
         System.out.println(logParser.getNumberOfUniqueIPs(null, new Date()));
         System.out.println(logParser.getUniqueIPs(null, new Date()));
-        System.out.println(logParser.getIPsForUser("Eduard Petrovich Morozko",null, new Date()));
+        System.out.println(logParser.getIPsForUser("Eduard Petrovich Morozko", null, new Date()));
         System.out.println(logParser.getIPsForEvent(Event.DONE_TASK, null, new Date()));
         System.out.println(logParser.getIPsForStatus(Status.OK, null, new Date()));
+
+        System.out.println("-- 2 -- ");
+        System.out.println(logParser.getAllUsers());
+        System.out.println(logParser.getNumberOfUsers(null, new Date()));
+        System.out.println(logParser.getNumberOfUserEvents("Eduard Petrovich Morozko", null, new Date()));
+        System.out.println(logParser.getUsersForIP("146.34.15.5", null, new Date()));
+        System.out.println(logParser.getLoggedUsers(null, new Date()));
+        System.out.println(logParser.getDownloadedPluginUsers(null, new Date()));
+        System.out.println(logParser.getWroteMessageUsers(null, new Date()));
+        System.out.println(logParser.getSolvedTaskUsers(null, new Date()));
+        System.out.println(logParser.getSolvedTaskUsers(null, new Date()));
+        System.out.println(logParser.getDoneTaskUsers(null, new Date()));
+        System.out.println(logParser.getDoneTaskUsers(null, new Date(), 48));
+
+        System.out.println("-- 3 -- ");
+        System.out.println(logParser.getDatesForUserAndEvent("Amigo", Event.SOLVE_TASK, null, null));
+        System.out.println(logParser.getDatesWhenSomethingFailed(null, null));
+        System.out.println(logParser.getDatesWhenErrorHappened(null, new Date()));
+        System.out.println(logParser.getDateWhenUserLoggedFirstTime("Amigo", null, new Date()));
+        System.out.println(logParser.getAllSolvedTasksAndTheirNumber(null, null));
+        System.out.println(logParser.getAllDoneTasksAndTheirNumber(null, null));
+
+        System.out.println("-- 4 --");
+        System.out.println(logParser.execute("get ip"));
+        System.out.println(logParser.execute("get user"));
+        System.out.println(logParser.execute("get date"));
+        System.out.println(logParser.execute("get event"));
+        System.out.println(logParser.execute("get status"));
+
+        System.out.println("-- 5 --");
+        System.out.println(logParser.execute("get event for date = \"30.08.2011 16:08:13\""));
+        System.out.println(logParser.execute("get ip for user = \"Eduard Petrovich Morozko\" and date between \"13.09.2013 5:04:49\" and \"13.09.2013 5:04:50\""));
+        System.out.println(logParser.execute("get ip for event = \"DONE_TASK\" and date between \"29.2.2028 5:4:7\" and \"29.2.2028 5:4:7\""));
+        System.out.println(logParser.execute("get status for user = \"Amigo\""));
+        System.out.println(logParser.execute("get event for ip = \"192.168.100.2\""));
 
     }
 }
