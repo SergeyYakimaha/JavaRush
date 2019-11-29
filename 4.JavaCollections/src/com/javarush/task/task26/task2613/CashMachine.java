42172d950092e844adde3aaea238bc7f528b8128
SergeyYakimaha <s.yakimaha78@gmail.com>
2019-11-29 14:46:54 +0200

    Commit

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/CashMachine.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CashMachine.java
@@ -1,7 +1,33 @@
 package com.javarush.task.task26.task2613;
 
+import java.util.Locale;
+
 public class CashMachine {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.ENGLISH);
+
+        CurrencyManipulator cmUSD = CurrencyManipulatorFactory.getManipulatorByCurrencyCode("USD");
+        CurrencyManipulator cmRUB = CurrencyManipulatorFactory.getManipulatorByCurrencyCode("RUB");
+        CurrencyManipulator cmUAH = CurrencyManipulatorFactory.getManipulatorByCurrencyCode("UAH");
+
+        CurrencyManipulator cmUSD1 = CurrencyManipulatorFactory.getManipulatorByCurrencyCode("USD");
+        CurrencyManipulator cmUSD2 = CurrencyManipulatorFactory.getManipulatorByCurrencyCode("USD");
+
+//        String currencyCode = ConsoleHelper.askCurrencyCode();
+//        String[] strings = ConsoleHelper.getValidTwoDigits(currencyCode);
+
+        //String[] usd_2_100 = ConsoleHelper.getValidTwoDigits(ConsoleHelper.askCurrencyCode());
+        //String[] usd_1_200 = ConsoleHelper.getValidTwoDigits(ConsoleHelper.askCurrencyCode());
+
+//        CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode).addAmount(Integer.parseInt(strings[0]),
+//                Integer.parseInt(strings[1]));
+//
+//        System.out.println(CurrencyManipulatorFactory.getManipulatorByCurrencyCode("USD").getTotalAmount());
+
+        ConsoleHelper.askOperation();
+
+
+
 
     }
 }
---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulator.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task26.task2613;
+
+import java.util.*;
+
+public class CurrencyManipulator {
+    private String currencyCode;
+
+    private Map<Integer, Integer> denominations = new HashMap<>();
+
+    public CurrencyManipulator(String currencyCode) {
+        this.currencyCode = currencyCode;
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+
+    public void addAmount(int denomination, int count) {
+        if (denominations.containsKey(denomination)) {
+            denominations.put(denomination, denominations.get(denomination) + count);
+        } else {
+            denominations.put(denomination, count);
+        }
+    }
+
+    public int getTotalAmount() {
+        int sum = 0;
+        for (Map.Entry<Integer, Integer> entry : denominations.entrySet())
+            sum += entry.getKey() * entry.getValue();
+        return sum;
+    }
+}
